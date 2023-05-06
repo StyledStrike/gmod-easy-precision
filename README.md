250f4674011d58e4e4639d6c48b16e2f55e9d3cc
StyledStrike <lameulucas@gmail.com>
2023-05-05 22:39:12 -0300

    Add existing project files

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # gmod-easy-precision
+
 Precisely move a prop to another with just two clicks.
+
+[![GLuaLint](https://github.com/StyledStrike/gmod-easy-precision/actions/workflows/glualint.yml/badge.svg)](https://github.com/FPtje/GLuaFixer)
+
+## Features
+
+This addon was inspired by the *"Move Entity"* feature from [Precision Alignment](https://steamcommunity.com/sharedfiles/filedetails/?id=457478322), but without the hassle of opening the menu to create constructs.
+
+- Move a prop to another (or to a place in the world) with just two clicks
+- *Reload* key to snap a prop to the grid
+- *(Optional)* Snap the cursor to the grid
+
+## Contributing
+
+Please follow the [CFC style guidelines](https://github.com/CFC-Servers/cfc_glua_style_guidelines) before opening pull requests.
---
+++ lua/weapons/gmod_tool/stools/easy_precision.lua
@@ -0,0 +1,295 @@
+TOOL.Category = "Construction"
+TOOL.Name = "#tool.easy_precision.name"
+
+TOOL.ClientConVar = {
+    follow_grid = "0",
+    grid_x = "10",
+    grid_y = "10",
+    grid_z = "1"
+}
+
+if CLIENT then
+    TOOL.Information = {
+        { name = "left", stage = 0 },
+        { name = "left_1", stage = 1 },
+        { name = "right", stage = 1 },
+        { name = "reload" }
+    }
+
+    language.Add( "tool.easy_precision.name", "Easy Precision" )
+    language.Add( "tool.easy_precision.desc", "Precisely move a prop to another with just two clicks" )
+
+    language.Add( "tool.easy_precision.left", "Select an object to move" )
+    language.Add( "tool.easy_precision.left_1", "Move object here" )
+    language.Add( "tool.easy_precision.right", "Cancel" )
+    language.Add( "tool.easy_precision.reload", "Snap a object's position/rotation to the grid" )
+
+    language.Add( "tool.easy_precision.follow_grid", "Snap cursor to the grid" )
+    language.Add( "tool.easy_precision.grid_x", "Grid Size (X)" )
+    language.Add( "tool.easy_precision.grid_y", "Grid Size (Y)" )
+    language.Add( "tool.easy_precision.grid_z", "Grid Size (Z)" )
+
+    language.Add( "tool.easy_precision.grid_z.help", [[The grid size has an effect on:
+- The Reload button
+- The cursor, if "Snap cursor to the grid" is enabled"]] )
+end
+
+local function Snap( num, grid )
+    return math.Round( num / grid ) * grid
+end
+
+local function GetAABBSize( ent )
+    local ret, ret2 = ent:GetPhysicsObject():GetAABB()
+    ret = ret or vector_origin
+    ret2 = ret2 or vector_origin
+    return ret2 - ret
+end
+
+local function GetCursorPosOnEntity( ent, from )
+    -- convert "from" to the ent's local coords,
+    -- but using the bounding box center as the origin
+
+    local boxCenter = ent:LocalToWorld( ent:OBBCenter() )
+    local pos, _ = WorldToLocal( from, angle_zero, boxCenter, ent:GetAngles() )
+
+    local size = GetAABBSize( ent )
+    local snapSize = size * 0.5
+
+    pos.x = Snap( pos.x, snapSize.x )
+    pos.y = Snap( pos.y, snapSize.y )
+    pos.z = Snap( pos.z, snapSize.z )
+
+    -- convert the local pos to world,
+    -- but using the bounding box center as the origin
+    pos, _ = LocalToWorld( pos, angle_zero, boxCenter, ent:GetAngles() )
+
+    return pos
+end
+
+local function GetTraceEntity( trace )
+    local ent = trace.Entity
+
+    if IsValid( ent ) and not ent:IsPlayer() and not ent:IsRagdoll() then
+        local phys = ent:GetPhysicsObject()
+
+        if IsValid( phys ) then
+            return ent
+        end
+    end
+end
+
+function TOOL:GetCursorPos()
+    local trace = self:GetOwner():GetEyeTrace()
+
+    if trace.Hit then
+        local pos = trace.HitPos
+        local ent = GetTraceEntity( trace )
+
+        if IsValid( ent ) then
+            pos = GetCursorPosOnEntity( ent, pos )
+
+        elseif self:GetClientNumber( "follow_grid", 0 ) > 0 then
+            pos.x = Snap( pos.x, self:GetClientNumber( "grid_x", 10 ) )
+            pos.y = Snap( pos.y, self:GetClientNumber( "grid_y", 10 ) )
+            pos.z = Snap( pos.z, self:GetClientNumber( "grid_z", 1 ) )
+        end
+
+        return pos
+    end
+
+    return trace.StartPos
+end
+
+function TOOL:SetCursorColor( color )
+    if IsValid( self.cursorEnt ) then
+        self.cursorEnt:SetColor( color )
+    end
+end
+
+function TOOL:ResetStage()
+    self:SetStage( 0 )
+
+    if SERVER then
+        if IsValid( self.moveGhost ) then
+            self.moveGhost:Remove()
+        end
+
+        self.moveGhost = nil
+        self.moveEntity = nil
+        self.moveOffset = nil
+        self.moveAngles = nil
+
+        self:SetCursorColor( color_white )
+    end
+end
+
+function TOOL:Think()
+    if self.moveEntity and not IsValid( self.moveEntity ) then
+        self:ResetStage()
+        return
+    end
+
+    if not SERVER then return end
+
+    local cursorPos = self:GetCursorPos()
+
+    if IsValid( self.cursorEnt ) then
+        self.cursorEnt:SetPos( cursorPos )
+    end
+
+    if IsValid( self.moveGhost ) then
+        local localCursor = self.moveGhost:WorldToLocal( cursorPos )
+        local offset = localCursor - self.moveOffset
+
+        self.moveGhost:SetPos( self.moveGhost:LocalToWorld( offset ) )
+    end
+end
+
+function TOOL:Deploy()
+    -- we dont use the cursor clientside because
+    -- its location might depend on a physobj
+    if SERVER then
+        local ent = ents.Create( "prop_physics" )
+        if not IsValid( ent ) then return end
+
+        ent:SetModel( "models/editor/axis_helper.mdl" )
+        ent:SetPos( Vector() )
+        ent:SetAngles( angle_zero )
+        ent:Spawn()
+
+        ent:PhysicsDestroy()
+        ent:SetMoveType( MOVETYPE_NONE )
+        ent:SetNotSolid( true )
+        ent:SetRenderMode( RENDERMODE_NORMAL )
+        ent:SetMaterial( "debug/debugdrawflat" )
+        ent:SetColor( color_white )
+        ent:DrawShadow( false )
+
+        self.cursorEnt = ent
+    end
+end
+
+function TOOL:Holster()
+    self:ResetStage()
+
+    if SERVER then
+        if IsValid( self.cursorEnt ) then
+            self.cursorEnt:Remove()
+        end
+
+        self.cursorEnt = nil
+    end
+end
+
+function TOOL:LeftClick( trace )
+    local stage = self:GetStage()
+    local ent = GetTraceEntity( trace )
+
+    if stage > 0 then
+        if SERVER then
+            local localCursor = self.moveEntity:WorldToLocal( self:GetCursorPos() )
+            local offset = localCursor - self.moveOffset
+
+            self.moveEntity:SetPos( self.moveEntity:LocalToWorld( offset ) )
+            self.moveEntity:SetAngles( self.moveAngles )
+        end
+
+        self:ResetStage()
+
+        return true
+    end
+
+    if not ent then
+        -- almost correct feedback on the client
+        if CLIENT and IsValid( trace.Entity ) then
+            return true
+        end
+
+        return
+    end
+
+    self:SetStage( 1 )
+
+    if SERVER then
+        local cursorPos = GetCursorPosOnEntity( ent, trace.HitPos )
+        local offset = ent:WorldToLocal( cursorPos )
+
+        local ghost = ents.Create( "prop_physics" )
+        ghost:SetModel( ent:GetModel() )
+        ghost:SetPos( ent:GetPos() )
+        ghost:SetAngles( ent:GetAngles() )
+        ghost:Spawn()
+
+        ghost:PhysicsDestroy()
+        ghost:SetMoveType( MOVETYPE_NONE )
+        ghost:SetNotSolid( true )
+        ghost:SetRenderMode( RENDERMODE_TRANSCOLOR )
+        ghost:SetColor( Color( 255, 255, 255, 150 ) )
+        ghost:DrawShadow( false )
+
+        self.moveGhost = ghost
+        self.moveEntity = ent
+        self.moveOffset = offset
+        self.moveAngles = ent:GetAngles()
+
+        self:SetCursorColor( Color( 0, 255, 255 ) )
+    end
+
+    return true
+end
+
+function TOOL:RightClick()
+    if self:GetStage() > 0 then
+        self:ResetStage()
+
+        return true
+    end
+end
+
+function TOOL:Reload( trace )
+    local ent = GetTraceEntity( trace )
+
+    if not ent then
+        -- almost correct feedback on the client
+        if CLIENT and IsValid( trace.Entity ) then
+            return true
+        end
+
+        return
+    end
+
+    if SERVER then
+        local phys = ent:GetPhysicsObject()
+
+        if IsValid( phys ) then
+            phys:EnableMotion( false )
+        end
+
+        local ang = ent:GetAngles()
+
+        ent:SetAngles( Angle(
+            Snap( ang.pitch, 10 ),
+            Snap( ang.yaw, 10 ),
+            Snap( ang.roll, 10 )
+        ) )
+
+        local pos = ent:GetPos()
+
+        pos.x = Snap( pos.x, self:GetClientNumber( "grid_x", 10 ) )
+        pos.y = Snap( pos.y, self:GetClientNumber( "grid_y", 10 ) )
+        pos.z = Snap( pos.z, self:GetClientNumber( "grid_z", 1 ) )
+
+        ent:SetPos( pos )
+    end
+
+    return true
+end
+
+function TOOL.BuildCPanel( p )
+    p:AddControl( "Header", { Description = "#tool.easy_precision.desc" } )
+    p:AddControl( "CheckBox", { Label = "#tool.easy_precision.follow_grid", Command = "easy_precision_follow_grid" } )
+
+    p:AddControl( "Slider", { Label = "#tool.easy_precision.grid_x", Command = "easy_precision_grid_x", Type = "Float", Min = 0.01, Max = 1000 } )
+    p:AddControl( "Slider", { Label = "#tool.easy_precision.grid_y", Command = "easy_precision_grid_y", Type = "Float", Min = 0.01, Max = 1000 } )
+    p:AddControl( "Slider", { Label = "#tool.easy_precision.grid_z", Command = "easy_precision_grid_z", Type = "Float", Min = 0.01, Max = 1000, Help = true } )
+end
